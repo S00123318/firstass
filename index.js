3f2f0a8ac32ca61fed9ef47fb0cad39c7a0c4b15
Dale <S00123318@mail.itsligo.ie>
2021-10-04 20:17:56 +0100

    task3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Joi = require('joi')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); //Parse URL-encoded bodies
@@ -9,41 +10,123 @@ app.use(express.urlencoded({extended: false})); //Parse URL-encoded bodies
 app.get('/', (req, res) => res.send('Hello World from dale!'))
 app.get('/chelsea', (req, res) => res.send('Chelsea won the champions league!'))
 
-let books = [];
+let books = [
+
+    {
+        "bookId":1,
+        "name": "Gansta Granny",
+        "quantity": 3
+    },
+    {
+        "bookId": 2,
+        "name": "The Boy in the Dress",
+        "quantity":2
+    },
+    {
+        "bookId": 3,
+        "name": "Bad Dad",
+    },
+];
 
 app.post('/books', (req, res) => {
-    const book = req.body;
 
-    const bookNumber = books.length;
+  const newBookId = books.length;
 
-    books.push(book);
+  const book = { bookId: newBookId, ...req.body };
 
-    res.location(`/books/${bookNumber}`).status(201).json(book);
+  const result = validateBook(req.body)
 
-   // res.send ('book has been added to the database');
-    // console.log(`book name is ${book.name} number of book is ${books.length}`);
+  if (result.error)
+  {
+    res.status(400).json(result.error);
+    return;
+  }
+
+
+  books.push(book);
+
+  res.location(`/books/${newBookId}`)
+    .status(201)
+    .json(book);
+
+
+  console.log(`book name is ${book.name} number of book(s) is ${books.length}`);
 
 });
 
 app.get('/books', (req, res) => {
-    res.send(books);
+  res.json(books);
 })
 
-app.get('/books/:id', (req,res) => {
 
-    let id = req.params.id;
-     res.json(null).status(404).json(books[id]);
- })
+app.get('/books/:id', (req, res) => {
 
- app.delete('/books/:id',(req, res) =>
- {
-    let id = req.params.id; 
-    console.log(`removing book ${books[id].name}`)
-    books.splice(req.params.id, 1);
-    res.send(books);
+  const id = req.params.id;
 
-  }) 
+  const book = books.find(b => b.bookId === parseInt(req.params.id))
+
+  if (!book) {
+    res.status(404);
+    res.json({ error: 'not found' });
+    return
+  }
+
+  res.json(book);
+})
 
 
+app.delete('/books/:id', (req, res) => {
+
+  const id = req.params.id;
+
+  const book = books.find(b => b.bookId === parseInt(req.params.id))
+
+  if (!book) {
+    res.status(404).json(`book with that ID {id} was not found`);
+    return;
+  }
+
+
+  const index = books.indexOf(book);
+
+  books.splice(index, 1);
+  res.send(book);
+
+})
+
+app.put('/books/:id', (req, res) => {
+
+  const id = req.params.id;
+
+  const result = validateBook(req.body)
+
+  if (result.error)
+  {
+    res.status(400).json(result.error);
+    return;
+  }
+
+  const book = books.find(b => b.bookId === parseInt(req.params.id))
+
+  if (!book) {
+    res.status(404).json(`book with that ID {req.params.id} was not found`);
+    return;
+  }
+
+  console.log(`changing book ${book.name}`);
+  book.name = req.body.name;
+  book.quantity = req.body.quantity;
+
+  res.send(book);
+
+})
+
+function validateBook(book) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    quantity: Joi.number().integer().min(0)
+  })
+  return schema.validate(book);
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
